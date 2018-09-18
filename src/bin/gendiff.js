ce33d2eecf55690aa24bf68ec71b780da178d446
roman <roman@lol.book>
2018-09-18 16:26:27 +0300

    updates node_modules

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const Program = require('commander');
+import * as commander from 'commander';
 
-Program
+commander
   .version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
+  .args('<firstConfig> <secondConfig>')
   .option('-h, --help', 'output usage information')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format')
