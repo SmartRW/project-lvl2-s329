1cb85e7ef93e1af5843843397b364e5f060e1faf
roman <roman@lol.book>
2018-09-18 17:34:14 +0300

    adds parsing arguments

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-import * as commander from 'commander';
+import commander from 'commander';
+import { description, version } from '../../package.json';
 
 commander
-  .version('0.0.3')
-  .description('Compares two configuration files and shows a difference.')
-  .args('<firstConfig> <secondConfig>')
-  .option('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
+  .version(`${version}`, '-V, --version')
+  .description(`${description}`)
+  .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
