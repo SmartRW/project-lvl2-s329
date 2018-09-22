7c981657962da115fd11db40eb7d088388a061e1
roman <roman@lol.book>
2018-09-22 20:54:09 +0300

    adds JSON output type, updates help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,7 +8,11 @@ commander
   .version(`${version}`, '-V, --version')
   .description(`${description}`)
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format')
+  .option('-f, --format [type]',
+    `Output format:
+    "tree" (default) - shows diff as a tree;
+    "plain" - shows diff as a list;
+    "json" - shows diff as a JSON string`)
   .action((firstConfig, secondConfig) => {
     console.log(genDiff(firstConfig, secondConfig, commander.format));
   })
--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import renderToTree from './renderToTree';
 import renderToPlain from './renderToPlain';
+import renderToJSON from './renderToJSON';
 
 const renderersTypes = {
   tree: renderToTree,
   plain: renderToPlain,
+  json: renderToJSON,
 };
 
 export default (data, type) => renderersTypes[type](data);
