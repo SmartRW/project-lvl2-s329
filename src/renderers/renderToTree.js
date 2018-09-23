54a828b2c271bafc50c1e3536fadcae2c835de16
roman <roman@lol.book>
2018-09-23 22:07:00 +0300

    test commit

--- src/renderers/renderToTree.js
+++ src/renderers/renderToTree.js
@@ -6,23 +6,18 @@ const render = (ast) => {
   const iter = (tree, treeLevel = 0) => {
     const indentBeforeNodeValue = ' '.repeat(singleIndentSpacesCount + treeLevel * singleIndentSpacesCount * 2);
     const indentBeforeEndBrace = ' '.repeat(treeLevel * singleIndentSpacesCount * 2);
-    const indentBeforeObject = treeLevel === 0
-      ? ' '.repeat(singleIndentSpacesCount * 3)
-      : indentBeforeNodeValue;
-
+    const indentBeforeObject = treeLevel === 0 ? ' '.repeat(singleIndentSpacesCount * 3) : indentBeforeNodeValue;
     const objectToString = (obj) => {
       const keys = Object.keys(obj);
       return keys
         .map(key => (_.isObject(obj[key]) ? objectToString(obj[key]) : `${indentBeforeObject}${key}: ${obj[key]}`))
         .join('\n');
     };
-
     const stringify = (value) => {
       const objectCase = `{\n${indentBeforeNodeValue}${objectToString(value)}\n  ${indentBeforeNodeValue}}`;
       const primitiveCase = `${value}`;
       return _.isObject(value) ? objectCase : primitiveCase;
     };
-
     const result = tree.map((element) => {
       const nodeCases = {
         ancestor: node => `${indentBeforeNodeValue}  ${node.key}: ${iter(node.children, treeLevel + 1)}`,
@@ -32,7 +27,6 @@ const render = (ast) => {
           `${indentBeforeNodeValue}- ${node.key}: ${stringify(node.previousValue)}`],
         unchanged: node => `${indentBeforeNodeValue}  ${node.key}: ${stringify(node.value)}`,
       };
-
       const getCase = node => nodeCases[node.type];
       return getCase(element)(element);
     });
