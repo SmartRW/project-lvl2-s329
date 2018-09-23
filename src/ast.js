d61433b472bdf3833891b9e5383de570b0532647
roman <roman@lol.book>
2018-09-23 22:01:49 +0300

    refactors buildAST from 'if' to polymorph

--- src/ast.js
+++ src/ast.js
@@ -1,46 +1,75 @@
 import _ from 'lodash';
 
+const nodeActions = [
+  {
+    check: (...rest) => {
+      const [,, value1, value2] = rest;
+      return _.isObject(value1) && _.isObject(value2);
+    },
+    action: (key, value1, value2, func) => ({
+      key,
+      type: 'ancestor',
+      children: func(value1, value2),
+    }),
+  },
+  {
+    check: (...rest) => {
+      const [data1,,,, key] = rest;
+      return !_.has(data1, key);
+    },
+    action: (key, value1, value2) => ({
+      key,
+      type: 'added',
+      value: value2,
+    }),
+  },
+  {
+    check: (...rest) => {
+      const [, data2,,, key] = rest;
+      return !_.has(data2, key);
+    },
+    action: (key, value1) => ({
+      key,
+      type: 'removed',
+      value: value1,
+    }),
+  },
+  {
+    check: (...rest) => {
+      const [,, value1, value2] = rest;
+      return value1 !== value2;
+    },
+    action: (key, value1, value2) => ({
+      key,
+      type: 'changed',
+      previousValue: value1,
+      value: value2,
+    }),
+  },
+  {
+    check: (...rest) => {
+      const [,, value1, value2] = rest;
+      return value1 === value2;
+    },
+    action: (key, value1) => ({
+      key,
+      type: 'unchanged',
+      value: value1,
+    }),
+  },
+];
+
 const buildAST = (firstData, secondData) => {
   const firstDataKeys = Object.keys(firstData);
   const secondDataKeys = Object.keys(secondData);
   const unitedKeys = _.union(firstDataKeys, secondDataKeys);
+
   const ast = unitedKeys.map((key) => {
     const firstDataValue = firstData[key];
     const secondDataValue = secondData[key];
-    if (_.isObject(firstDataValue) && _.isObject(secondDataValue)) {
-      return {
-        key,
-        type: 'ancestor',
-        children: buildAST(firstDataValue, secondDataValue),
-      };
-    }
-    if (!_.has(firstData, key)) {
-      return {
-        key,
-        type: 'added',
-        value: secondDataValue,
-      };
-    }
-    if (!_.has(secondData, key)) {
-      return {
-        key,
-        type: 'removed',
-        value: firstDataValue,
-      };
-    }
-    if (firstDataValue !== secondDataValue) {
-      return {
-        key,
-        type: 'changed',
-        previousValue: firstDataValue,
-        value: secondDataValue,
-      };
-    }
-    return {
-      key,
-      type: 'unchanged',
-      value: firstDataValue,
-    };
+    const getAction = nodeActions
+      .find(({ check }) => check(firstData, secondData, firstDataValue, secondDataValue, key));
+    return getAction.action(key, firstDataValue, secondDataValue, buildAST);
   });
   return ast;
 };
